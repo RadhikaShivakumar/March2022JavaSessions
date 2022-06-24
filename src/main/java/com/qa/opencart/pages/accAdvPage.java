c87dc62205b24ba3ecf8f2e04c7104d37634fbee
radhikashivakumar <radhikasham007@gmail.com>
2022-06-23 21:10:09 -0400

    added accAdv page to accpage branch

---
+++ src/main/java/com/qa/opencart/pages/accAdvPage.java
@@ -0,0 +1,9 @@
+package com.qa.opencart.pages;
+
+public class accAdvPage {
+
+	public void method1() {
+		System.out.println("This is for creating new file to create branch accpage in GIT");
+	}
+
+}
