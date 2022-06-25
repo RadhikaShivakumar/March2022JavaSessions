2e79a0bc89023527e38cddd4ac56175645683865
radhikashivakumar <radhikasham007@gmail.com>
2022-06-24 22:28:02 -0400

    deleted files

--- src/main/java/com/qa/opencart/pages/accAdvPage.java
+++
@@ -1,9 +0,0 @@
-package com.qa.opencart.pages;
-
-public class accAdvPage {
-
-	public void method1() {
-		System.out.println("This is for creating new file to create branch accpage in GIT");
-	}
-
-}
