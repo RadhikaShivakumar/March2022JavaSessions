50944773ca6c988bae5b5cb00195bf986908bd13
radhikashivakumar <radhikasham007@gmail.com>
2022-06-26 14:52:42 -0400

    Deleted space in login page

--- src/main/java/com/qa/opencart/pages/LoginPage.java
+++ src/main/java/com/qa/opencart/pages/LoginPage.java
@@ -20,8 +20,7 @@ public class LoginPage {
 	private By forgotPwdLink = By.linkText("Forgotten Password");
 	private By registerLink = By.linkText("Register");
 	private By logoutSuccessMsg = By.cssSelector("div#common-success h1"); 
-	
-	
+		
 	
 	//2. Page constructor...
 	public LoginPage(WebDriver driver) {
