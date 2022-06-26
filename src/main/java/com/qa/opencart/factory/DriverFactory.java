193511ce3101a1355af4fabc34044aa29ee4c698
radhikashivakumar <radhikasham007@gmail.com>
2022-06-26 16:48:28 -0400

    changed browser config to read from properties file

--- src/main/java/com/qa/opencart/factory/DriverFactory.java
+++ src/main/java/com/qa/opencart/factory/DriverFactory.java
@@ -41,11 +41,11 @@ public class DriverFactory {
 	 */
 	public WebDriver init_driver(Properties prop) {
 		
-		//String browserName = prop.getProperty("browser").trim();
+		String browserName = prop.getProperty("browser").trim();
 		
 		//for jenkins trigger
 		// mvn clean install -Denv="qa" -Dbrowser="chrome"
-		String browserName = System.getProperty("browser");
+		//String browserName = System.getProperty("browser"); //For jenkins selection of browser
 		
 		optionsManager= new OptionsManager(prop);
 		
